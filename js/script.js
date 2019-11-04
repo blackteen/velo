fa96295b79d3a374255528005bcf0e44b9c465f8
Yuriy Malarchuk <y.malarchuk@viseven.com>
2019-11-04 10:59:39 +0200

    update(): js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//dropdown
 (function () {
     let openBrand = document.getElementById('openBrand')
     if (openBrand) {
@@ -8,6 +9,7 @@
     }
 })();
 
+//dropdown
 (function () {
     let links = document.querySelectorAll(".filter-link");
     let button = document.getElementById('more');
@@ -22,38 +24,47 @@
         }, false)
     }
 })();
-$(document).ready(function(){
-    let toCard = function(){
-        $(".buy-button").click(function(){
-            let src =$(this).parents(".catalog-elem").find("img").attr("src")
-            let name =$(this).parents(".catalog-elem").find(".name").text()
-            $(".pop-img").attr("src",src)
+
+$(document).ready(function () {
+    //add to card popup
+    let toCard = function () {
+        $(".buy-button").click(function () {
+            let src = $(this).parents(".catalog-elem").find("img").attr("src")
+            let name = $(this).parents(".catalog-elem").find(".name").text()
+            $(".pop-img").attr("src", src)
             $(".popup-name").text(name)
             openPopup()
         })
-        $(".close-button-buy, .continue-buy").click(function(){
+        $(".close-button-buy, .continue-buy").click(function () {
             closePopup()
         })
-        function openPopup(){
+
+        function openPopup() {
             $("#buyPopup").fadeIn()
-        }  
-        function closePopup(){
+        }
+
+        function closePopup() {
             $("#buyPopup").fadeOut()
         }
-       
-    }
 
+    }
     toCard()
+
+    //dropdown
     $('.type-choose-catalog-element button').click(function () {
         $(this).parents().siblings(".type-choose-catalog-element").removeClass('opened');
         $(this).parents().siblings(".type-choose-catalog-element").children().children().children().removeClass('opened');
         $(this).parents(".type-choose-catalog-element").toggleClass('opened');
         $(this).toggleClass('opened');
     });
+
+    //callback popup
     $('.open-popup-link').magnificPopup({
-        type:'inline',
+        type: 'inline',
         midClick: true
     });
+
+    //homepage carousel
     $("#carousel").slick({
         centerMode: true,
         centerPadding: "5px",
